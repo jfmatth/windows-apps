931950fb3788744d737737f5f5b959c5c11cfffa
John Matthew <john@compunique.com>
2025-01-09 13:46:30 -0800

    minor update

--- installall.cmd
+++ installall.cmd
@@ -1,12 +1,14 @@
+winget install Microsoft.PowerShell
 winget install --scope machine 7zip.7zip
 winget install --scope machine PrivateInternetAccess.PrivateInternetAccess
 winget install --scope machine RedHat.VirtViewer
 winget install --scope machine RedHat.Podman
 winget install --scope machine Canonical.Multipass
-winget install --scope machine OpenWhisperSystems.Signal
-winget install --scope machine FluxCD.Flux
-winget install --scope machine Helm.Helm
-winget install --scope machine Hugo.Hugo
+winget install OpenWhisperSystems.Signal
+winget install FluxCD.Flux
+winget install Helm.Helm
+winget install Hugo.Hugo
 winget install --scope machine RedHat.Podman-Desktop
 winget install --scope machine Balena.Etcher
 winget install --scope machine Microsoft.WSL
+winget install python
