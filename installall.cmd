9f74a0798656dbaaaa6fe46173625d5878400703
John Matthew <john@compunique.com>
2025-01-09 13:24:45 -0800

    all programs

---
+++ installall.cmd
@@ -0,0 +1,12 @@
+winget install --scope machine 7zip.7zip
+winget install --scope machine PrivateInternetAccess.PrivateInternetAccess
+winget install --scope machine RedHat.VirtViewer
+winget install --scope machine RedHat.Podman
+winget install --scope machine Canonical.Multipass
+winget install --scope machine OpenWhisperSystems.Signal
+winget install --scope machine FluxCD.Flux
+winget install --scope machine Helm.Helm
+winget install --scope machine Hugo.Hugo
+winget install --scope machine RedHat.Podman-Desktop
+winget install --scope machine Balena.Etcher
+winget install --scope machine Microsoft.WSL
